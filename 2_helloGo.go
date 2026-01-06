daf223723e67c3636f74e33226f9504a47f34495
Isuru Shehara <isurushehara@gmail.com>
2026-01-06 18:39:26 +0530

    doc: Add detailed comments explaining the Hello World program

--- 2_helloGo.go
+++ 2_helloGo.go
@@ -4,3 +4,16 @@ import ("fmt")
 func main() {
   fmt.Println("Hello World!")
 }
+
+
+/*
+Line 1: In Go, every program is part of a package. We define this using the package keyword. In this example, the program belongs to the main package.
+
+Line 2: import ("fmt") lets us import files included in the fmt package.
+
+Line 3: A blank line. Go ignores white space. Having white spaces in code makes it more readable.
+
+Line 4: func main() {} is a function. Any code inside its curly brackets {} will be executed.
+
+Line 5: fmt.Println() is a function made available from the fmt package. It is used to output/print text. In our example it will output "Hello World!".
+*/
