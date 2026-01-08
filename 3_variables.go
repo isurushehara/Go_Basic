17263087b6eb1f597e193e16ef41df3fa2ae072f
Isuru Shehara <isurushehara@gmail.com>
2026-01-08 20:33:27 +0530

    feat: Add variables demonstration in Go

---
+++ 3_variables.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main() {
+
+  // var variablename type = value
+  var student1 string = "John" //type is string
+  var student2 = "Jane" //type is inferred
+  x := 2 //type is inferred
+
+  fmt.Println(student1)
+  fmt.Println(student2)
+  fmt.Println(x)
+}
