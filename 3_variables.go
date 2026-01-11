9a8cc5d28b19f9cd7fc019421646f1434d5e2951
Isuru Shehara <isurushehara@gmail.com>
2026-01-11 07:33:06 +0530

    Update(variables): Enhance comments for variable declaration examples in Go

--- 3_variables.go
+++ 3_variables.go
@@ -3,7 +3,17 @@ import ("fmt")
 
 func main() {
 
+  // Declaring (Creating) Variables
+
+  // 1. With the var keyword
   // var variablename type = value
+  var age int = 25
+
+  //2. With the := sign:
+  // variablename := value
+  name := "Alice"
+
+  //Example usages
   var student1 string = "John" //type is string
   var student2 = "Jane" //type is inferred
   x := 2 //type is inferred
