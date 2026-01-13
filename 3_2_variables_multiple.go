1d88f579b482ed506f2b10b346c1a45f84d50ebc
Isuru Shehara <isurushehara@gmail.com>
2026-01-13 21:40:58 +0530

    feat: Add initial implementation for multiple variable declaration in Go

---
+++ 3_2_variables_multiple.go
@@ -0,0 +1,15 @@
+package main
+import ("fmt")
+
+func main() {
+ // Go Multiple Variable Declaration
+  var a, b, c, d int = 1, 3, 5, 7
+
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+  fmt.Println(d)
+
+  // Go Variable Declaration in a Block
+  
+}
