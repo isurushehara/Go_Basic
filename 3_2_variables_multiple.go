d58b142ad7c1f008eb2a0d436d77131c5fac447e
Isuru Shehara <isurushehara@gmail.com>
2026-01-13 21:41:59 +0530

    style: Format code for better readability in variable declaration examples

--- 3_2_variables_multiple.go
+++ 3_2_variables_multiple.go
@@ -2,14 +2,23 @@ package main
 import ("fmt")
 
 func main() {
- // Go Multiple Variable Declaration
-  var a, b, c, d int = 1, 3, 5, 7
+	// Go Multiple Variable Declaration
+	var a, b, c, d int = 1, 3, 5, 7
 
-  fmt.Println(a)
-  fmt.Println(b)
-  fmt.Println(c)
-  fmt.Println(d)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
 
-  // Go Variable Declaration in a Block
-  
+	// Go Variable Declaration in a Block
+		var (
+		a int
+		b int = 1
+		c string = "hello"
+	)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
 }
