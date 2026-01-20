a7f1f519ae42fab69b41bc746fc5612a42fc2445
Isuru Shehara <isurushehara@gmail.com>
2026-01-20 12:14:48 +0530

    feat: Add example demonstrating constant immutability in 4_constants.go

--- 4_constants.go
+++ 4_constants.go
@@ -48,6 +48,23 @@ func main() {
   fmt.Println(A)
 }
   
+
+- Constants: Unchangeable and Read-only
+When a constant is declared, it is not possible to change the value later:
+
+Example
+package main
+import ("fmt")
+
+func main() {
+  const A = 1
+  A = 2
+  fmt.Println(A)
+}
+Result:
+./prog.go:8:7: cannot assign to A
+
+
 */
 
 
