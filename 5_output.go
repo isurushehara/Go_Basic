4c045c42678664c5075225b2e265b6aab19549ac
Isuru Shehara <isurushehara@gmail.com>
2026-01-26 18:02:09 +0530

    feat: Add examples demonstrating Print, Println, and Printf functions in 5_output.go

---
+++ 5_output.go
@@ -0,0 +1,54 @@
+/*
+Go has three functions to output text:
+
+Print()
+Println()
+Printf()
+*/
+
+// Print()
+/*
+package main
+import ("fmt")
+
+func main() {
+  var i,j string = "Hello","World"
+
+  fmt.Print(i)
+  fmt.Print(j)
+}
+*/
+
+// Println()
+// The Println() function is similar to Print() with the difference that a whitespace is added between the arguments, and a newline is added at the end:
+/*
+package main
+import ("fmt")
+
+func main() {
+  var i,j string = "Hello","World"
+
+  fmt.Println(i,j)
+}
+
+*/
+
+// Printf()
+/*
+The Printf() function first formats its argument based on the given formatting verb and then prints them.
+
+Here we will use two formatting verbs:
+
+%v is used to print the value of the arguments
+%T is used to print the type of the arguments
+*/
+package main
+import ("fmt")
+
+func main() {
+  var i string = "Hello"
+  var j int = 15
+
+  fmt.Printf("i has value: %v and type: %T\n", i, i)
+  fmt.Printf("j has value: %v and type: %T", j, j)
+}
